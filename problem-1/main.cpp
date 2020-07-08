aa24f4850541d9a0ad963b83acd33e2476c4d767
Patrick Tsang <patricksmtsang@gmail.com>
2020-07-08 12:46:01 -0400

    p1

--- problem-1/main.cpp
+++ problem-1/main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 
-using ll = long long;
-
 int main() {
-    ll count = 0;
-    for(int i = 1; i++; i < 1000){
+    int count = 0;
+    for(int i = 1; i < 1000; i++){
         if(i % 3 == 0 || i % 5 == 0){
-            count += i
+            count += i;
         }
     }
     std::cout << "Total: " << count;
+    return 0;
 }
